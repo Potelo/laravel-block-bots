3ffe344e39334950653161426d28c5ec0069d545
Andre Walker <andre.walkermo@gmail.com>
2023-01-11 19:23:04 -0300

    fix: remove tipo da propriedade, mas manter suporte abaixo do php7.4

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -19,7 +19,7 @@ class Client
     /**
      * @var \Potelo\LaravelBlockBots\Contracts\Configuration
      */
-    private Configuration $options;
+    private $options;
 
     public function __construct($request)
     {
